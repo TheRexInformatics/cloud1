20d4fc13a130c660aecef8f606ec8ab4c6954879
TheRexInformatics <ma.villarroelt@duocuc.cl>
2026-08-14 10:49:14 -0400

    V1.2.2

--- src/main/java/com/holamundo/v1/controller/HolaMundoV1Controller.java
+++ src/main/java/com/holamundo/v1/controller/HolaMundoV1Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/api/v1")
 public class HolaMundoV1Controller {
 
     // Este es un ejemplo de un endpoint GET que no recibe parámetros y retorna un saludo 
